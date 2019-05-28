7a6a607317bee0eb0031935c4bd07ccb74837768
Ademola Ariya <ademola.ariya@andela.com>
2019-05-27 20:37:35 -0700

    Add db url to .env

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./src/routes');
 
 const app = express();
 
-const connectionString = 'mongodb://localhost:27017/population_management_dev';
+const connectionString = process.env.DB_URL;
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to location Management!' });
